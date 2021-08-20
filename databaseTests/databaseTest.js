1f805b785ff11dfe70b9a46fa4fecb885f7e0444
Darin <wksesports@gmail.com>
2021-08-20 18:31:55 -0400

    exercise 4 commit

--- databaseTests/databaseTest.js
+++ databaseTests/databaseTest.js
@@ -3,6 +3,8 @@ const userSchema = require('./schemas/user-schema');
 const argv = require('yargs').argv;
 //The schema is just the set up of the collection in the database, so we have a separate js to create the setup we want to use
 
+//specific argv for exercise 4
+const input = argv._[0];
 
 console.log(argv)
 //this is just to check that it's getting our information the user is passing in from the console
@@ -27,4 +29,21 @@ const connectToMongoDB = async () => {
   })
 }
 
-connectToMongoDB()
+const fetchFromMongoDB = async () => {
+  await mongo().then(async(mongoose) => {
+    try {
+      console.log('Connected to mongodb!')
+
+//The only real change needed here was a separate function that would handle finding someone in the database using a certain metric, for this exercise it was the userId. 
+      const result = await userSchema.find({
+        userId: input});
+      console.log(result);
+
+    } finally {
+      mongoose.connection.close()
+    }
+  })
+}
+
+fetchFromMongoDB()
+//connectToMongoDB()
